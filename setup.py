64f3e90939f05023802aacfad3475e9bdd53349e
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-08-14 07:34:06 +0300

    fixed install for window

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ try:
 except:
     
     os.system("git clone https://github.com/XLabs-AI/x-flux" )
-#os.rename("x-flux", "xflux")
+#os.rename("x-flux", "xflux")    
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 run = f"mv x-flux {cur_dir}/xflux"
+if os.name == 'nt':
+    run = f"move x-flux {cur_dir}/xflux"
 os.system(run)
 os.system(f"pip install -r {cur_dir}/requirements.txt")
 os.system(f"pip install -r {cur_dir}/xflux/requirements.txt")
