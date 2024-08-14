44fb53a1a6e7af30980ee53a6219a083241c4c1b
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-08-14 12:30:21 +0300

    rm gitclone

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import os
-try:
-    import git  
-    git.Git(".").clone("https://github.com/XLabs-AI/x-flux")
-except:
-    
-    os.system("git clone https://github.com/XLabs-AI/x-flux" )
+if False:
+    try:
+        import git  
+        git.Git(".").clone("https://github.com/XLabs-AI/x-flux")
+    except:
+        
+        os.system("git clone https://github.com/XLabs-AI/x-flux" )
 #os.rename("x-flux", "xflux")    
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 run = f"mv x-flux {cur_dir}/xflux"
