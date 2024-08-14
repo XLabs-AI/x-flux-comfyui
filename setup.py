be9acb8e401e8863407f52ab6eb62d576fe9af9f
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-08-14 14:18:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ if False:
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 run = f"mv x-flux {cur_dir}/xflux"
 if os.name == 'nt':
-    run = f"move x-flux {cur_dir}/xflux"
+    run = f"move x-flux {cur_dir}\\xflux"
 os.system(run)
-os.system(f"pip install -r {cur_dir}/requirements.txt")
+if os.name == 'nt':
+    os.system(f"pip install -r {cur_dir}\\requirements.txt")
+else:
+    os.system(f"pip install -r {cur_dir}/requirements.txt")
 print("Succesfully installed")
