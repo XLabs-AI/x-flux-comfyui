c1ebe8993aa95f3860d864237b1bd0eea2e2b67a
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-08-14 03:15:42 +0300

    Fixed install

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ try:
     import git  
     git.Git(".").clone("https://github.com/XLabs-AI/x-flux")
 except:
+    
     os.system("git clone https://github.com/XLabs-AI/x-flux" )
 #os.rename("x-flux", "xflux")
-os.system("mv x-flux xflux")
-os.system("pip install -r requirements.txt")
-os.system("pip install -r xflux/requirements.txt")
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+run = f"mv x-flux {cur_dir}/xflux"
+os.system(run)
+os.system(f"pip install -r {cur_dir}/requirements.txt")
+os.system(f"pip install -r {cur_dir}/xflux/requirements.txt")
 print("Succesfully installed")
