8e8fef6198fcb17a2b55e813face7095a7ecfbde
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-08-14 03:04:49 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ try:
     git.Git(".").clone("https://github.com/XLabs-AI/x-flux")
 except:
     os.system("git clone https://github.com/XLabs-AI/x-flux" )
-os.rename("x-flux", "xflux")
+#os.rename("x-flux", "xflux")
+os.system("mv x-flux xflux")
 os.system("pip install -r requirements.txt")
 os.system("pip install -r xflux/requirements.txt")
 print("Succesfully installed")
