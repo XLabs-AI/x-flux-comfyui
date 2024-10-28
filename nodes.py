72e6d2f48ecd04030cde15762dc7b0b7c2b39c17
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2024-10-28 13:21:41 +0300

    fix: rm text_scale

--- nodes.py
+++ nodes.py
@@ -618,7 +618,7 @@ class ApplyFluxIPAdapter:
 
         ipad_blocks = []
         for block in ip_adapter_flux['double_blocks']:
-            ipad = IPProcessor(block.context_dim, block.hidden_dim, ip_projes, ip_scale, text_scale)
+            ipad = IPProcessor(block.context_dim, block.hidden_dim, ip_projes, ip_scale)
             ipad.load_state_dict(block.state_dict())
             ipad.in_hidden_states_neg = ip_neg_pr
             ipad.in_hidden_states_pos = ip_projes
